51d94b81c42b868285f66df4aa4cef0405e6d4f6
srinathgadde <119876373+srinathgadde@users.noreply.github.com>
2023-06-18 21:26:54 +0530

    Update Kadane's Algorithm-maximumSubArray.java

--- Kadane's Algorithm-maximumSubArray.java
+++ Kadane's Algorithm-maximumSubArray.java
@@ -1,1 +1,13 @@
-
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int sum=0,maxi=Integer.MIN_VALUE;
+        int i=0,j=0;
+        for(i=0;i<nums.length;i++){
+            sum=sum+nums[i];
+            maxi=Math.max(sum,maxi);
+            if(sum<0) sum=0;
+            
+        }
+        return maxi;
+    }
+}
